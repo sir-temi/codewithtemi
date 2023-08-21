58ab396b3ea1de1b5b4bbc71dff20083fc1ff2d3
TemiTope <siryankey@protonmail.com>
2023-08-21 19:38:37 +0100

    Fixed logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,29 +1,27 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
-import Logo from '../assets/logo.png'
-
+import Logo from "../assets/logo.svg";
 
 function Header({ maintenanceMode }: { maintenanceMode: boolean }) {
     return (
-        <Navbar expand="lg" className="" bg="primary" data-bs-theme="dark">
+        <Navbar expand="lg" className="" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand as={Link} to="/">
-                    <img src={Logo} alt="logo" height="30" />
+                    <img src={Logo} alt="logo" height="60" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    {!maintenanceMode && <Nav className="ms-auto">
-                        <NavLink
-                            to="/about"
-                            className="nav-link"
-                        >
-                            About Me
-                        </NavLink>
-                    </Nav>}
+                    {!maintenanceMode && (
+                        <Nav className="ms-auto">
+                            <NavLink to="/about" className="nav-link">
+                                About Me
+                            </NavLink>
+                        </Nav>
+                    )}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
 
-export default Header
+export default Header;
